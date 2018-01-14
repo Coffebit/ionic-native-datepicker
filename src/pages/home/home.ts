c33d9a6df1d2b31bc5a5128c1d3f49396c5923e6
Milton Câmara <miltomcamara@gmail.com>
2018-01-14 13:19:15 -0200

    Initial Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { DatePicker } from '@ionic-native/date-picker';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(private datePicker: DatePicker) {
+
+  }
+
+  openNativeDatePicker() {
+    this.datePicker.show({
+      date: new Date(),
+      mode: 'date',
+      androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
+    }).then(
+      date => console.log('Got date: ', date),
+      err => console.log('Error occurred while getting date: ', err)
+    );
+  }
+
+}
